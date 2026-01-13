f6004364ad10aa6df891be866c68185ff27af4b0
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2026-01-13 22:26:56 +0100

    simple testbench

--- i2s_demo.srcs/sim_1/new/tb_i2s_tx.v
+++ i2s_demo.srcs/sim_1/new/tb_i2s_tx.v
@@ -3,32 +3,39 @@
 
 module tb_i2s_tx;
 
-  reg bit_clk, reset_in;
+  reg bit_clk, bit_clk_locked, reset;
   wire lr_clk, data;
 
   i2s_tx #(
-      .SAMPLE_BIT_DEPTH(32)
-  ) i2s_tx_instance (
+      .DATA_BIT_DEPTH(32)
+  ) i2s_tx_inst (
       .bit_clk_in(bit_clk),
-      .reset_in(reset_in),
+      .bit_clk_locked_in(bit_clk_locked),
+      .reset_in(reset),
       .bit_clk_out(),
       .lr_clk_out(lr_clk),
       .data_out(data)
   );
 
-  initial bit_clk = 0;
+  // generate clock
+  initial bit_clk = 'b0;
   always #10 bit_clk = ~bit_clk;
 
+  // initial reset and PLL lock
   initial begin
-    reset_in = 'b1;
+    reset = 'b0;
+    bit_clk_locked = 'b0;
     #50;
-    reset_in = 'b0;
+    reset = 'b1;
+    #50;
+    bit_clk_locked = 'b1;
   end
 
+  // dump simulation
   initial begin
     $dumpfile("i2s_tx.vcd");
     $dumpvars(0, tb_i2s_tx);
-    #10000;
+    #100000;
     $finish;
   end
 
