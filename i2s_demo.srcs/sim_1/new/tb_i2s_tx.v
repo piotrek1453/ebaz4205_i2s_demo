1df7dec9dbe3512d6ac6ab653249d9cca0316693
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2026-01-17 22:14:00 +0100

    moving loading data to tx outside of its module

--- i2s_demo.srcs/sim_1/new/tb_i2s_tx.v
+++ i2s_demo.srcs/sim_1/new/tb_i2s_tx.v
@@ -3,7 +3,8 @@
 
 module tb_i2s_tx;
 
-  reg bit_clk_in_reg, bit_clk_locked_in_reg, reset_in_reg;
+  reg bit_clk_in_reg, bit_clk_locked_in_reg, reset_in_reg, audio_enable_in_reg;
+  reg [31:0] left_sample_in_reg, right_sample_in_reg;
   wire lr_clk_out_reg, data_out_reg, bit_clk_out_reg;
 
   i2s_tx #(
@@ -12,6 +13,9 @@ module tb_i2s_tx;
       .bit_clk_in(bit_clk_in_reg),
       .bit_clk_locked_in(bit_clk_locked_in_reg),
       .reset_in(reset_in_reg),
+      .audio_enable_in(audio_enable_in_reg),
+      .left_sample_in(left_sample_in_reg),
+      .right_sample_in(right_sample_in_reg),
       .bit_clk_out(bit_clk_out_reg),
       .lr_clk_out(lr_clk_out_reg),
       .data_out(data_out_reg)
@@ -23,12 +27,20 @@ module tb_i2s_tx;
 
   // initial reset and PLL lock
   initial begin
+    left_sample_in_reg = 'b0;
+    right_sample_in_reg = 'b0;
     reset_in_reg = 'b0;
     bit_clk_locked_in_reg = 'b0;
+    audio_enable_in_reg = 'b1;
     #50;
     reset_in_reg = 'b1;
     #50;
     bit_clk_locked_in_reg = 'b1;
+  end
+
+  always @(posedge bit_clk_in_reg) begin
+    left_sample_in_reg  <= left_sample_in_reg + 'd1000000;
+    right_sample_in_reg <= right_sample_in_reg - 'd1000000;
   end
 
   // dump simulation
--- i2s_demo.srcs/sources_1/new/i2s_tx.v
+++ i2s_demo.srcs/sources_1/new/i2s_tx.v
@@ -6,48 +6,74 @@ module i2s_tx #(
     parameter integer DATA_BIT_DEPTH = 32
 ) (
     // speed of this clock is dependent on transmission parameters: feed as per slave's manual
-    input  wire bit_clk_in,
+    input wire bit_clk_in,
     // either connect to PLL's locked output to wait for stable clock or wire to '1'
-    input  wire bit_clk_locked_in,
-    input  wire reset_in,
-    input  wire audio_enable_in,
+    input wire bit_clk_locked_in,
+    input wire reset_in,
+    input wire audio_enable_in,
+    input wire [DATA_BIT_DEPTH-1:0] left_sample_in,
+    input wire [DATA_BIT_DEPTH-1:0] right_sample_in,
     output wire bit_clk_out,
     output wire lr_clk_out,
     output wire data_out
 );
   localparam integer DATA_MSB_IDX = DATA_BIT_DEPTH - 1;
 
-  reg lr_clk_reg, data_reg, audio_enable_reg;
-  // reg notation is [msb:lsb] and i2s starts with MSB so start from bit DATA_BIT_DEPTH-1
-  reg [DATA_MSB_IDX:0] sample_buffer;
+  reg lr_clk_reg, current_sample_bit_reg, audio_enable_reg;
+  // reg notation is [msb:lsb] and i2s starts with MSB so start from bit
+  // DATA_MSB_IDX
+  reg [DATA_MSB_IDX:0] left_sample_reg, right_sample_reg, current_channel_sample_reg;
   integer current_bit_index;
 
   assign bit_clk_out = bit_clk_in;
   assign lr_clk_out = lr_clk_reg;
-  assign data_out = data_reg & audio_enable_reg;
+  assign data_out = current_sample_bit_reg & audio_enable_reg;
 
   initial begin
     lr_clk_reg = 'b0;  // start with left channel
-    data_reg = 'b0;  // reset output
-    sample_buffer = 'd0;  // test samples
-    // reset bit counter to DATA_BIT_DEPTH-1: going from MSB to LSB
+    current_sample_bit_reg = 'b0;  // reset output
+    // reset bit counter to DATA_MSB_IDX: going from MSB to LSB
     current_bit_index = DATA_MSB_IDX;
     audio_enable_reg = 'b1;
+    right_sample_reg = 'b0;
+    left_sample_reg = 'b0;
   end
 
+  // toggle audio output on button press
   always @(negedge audio_enable_in) begin
     if (!audio_enable_in) begin
       audio_enable_reg <= ~audio_enable_reg;
     end
   end
 
+  // Synchronizacja wejść audio
+  always @(posedge bit_clk_in) begin
+    if (bit_clk_locked_in) begin
+      left_sample_reg  <= left_sample_in;
+      right_sample_reg <= right_sample_in;
+    end
+  end
+
+  always @(posedge bit_clk_in) begin
+    // after last bit sent load corresponding channel's sample
+    if (current_bit_index == 0) begin
+      if (lr_clk_reg == 'b0) begin
+        current_channel_sample_reg <= left_sample_reg;
+      end else if (lr_clk_reg == 'b1) begin
+        current_channel_sample_reg <= right_sample_reg;
+      end
+    end
+  end
+
   always @(negedge bit_clk_in) begin
     if (!reset_in || !bit_clk_locked_in) begin
       lr_clk_reg <= 'b0;
-      data_reg <= 'b0;
+      current_sample_bit_reg <= 'b0;
       current_bit_index <= DATA_MSB_IDX;
+      right_sample_reg <= 'b0;
+      left_sample_reg <= 'b0;
     end else begin
-      data_reg <= sample_buffer[current_bit_index];
+      current_sample_bit_reg <= current_channel_sample_reg[current_bit_index];
       if (current_bit_index == 0) begin
         lr_clk_reg <= ~lr_clk_reg;
       end
@@ -55,7 +81,6 @@ module i2s_tx #(
 
     if (current_bit_index == 0) begin
       current_bit_index <= DATA_MSB_IDX;
-      sample_buffer <= sample_buffer + 10000000;  // invert test samples for other channel
     end else begin
       current_bit_index <= current_bit_index - 1;
     end
